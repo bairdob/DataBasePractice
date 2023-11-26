d2ad822c05aeeca882d080449915c0bf7636e8e3
bairdob <krumane@mail.ru>
2023-11-26 23:07:09 +0300

    use server exception

--- lr4/task4.sh
+++ lr4/task4.sh
@@ -2,21 +2,22 @@
 psql postgresql://postgres@localhost<< EOF
 DO \$\$
 DECLARE
-    v_rating_p hr_poc1.Products.rating_p%TYPE := 0;
+    v_rating_p hr_poc1.Products.rating_p%TYPE := 6;
 BEGIN
     INSERT INTO hr_poc1.products(
         product_id, product_name, rating_p, price)
     VALUES(87, 'super_fruit', v_rating_p, 10);
+    RAISE NOTICE 'Недопустимое значение rating_p=%', v_rating_p;
 
     EXCEPTION
     WHEN SQLSTATE '23514' THEN
-        v_rating_p = v_rating_p + 1;
+        v_rating_p = 1;
 
         INSERT INTO hr_poc1.products(
             product_id, product_name, rating_p, price)
         VALUES(87, 'super_fruit', v_rating_p, 10);
         
-        RAISE NOTICE 'Операция выполнена, rating_p увеличен на 1, rating_p=%', 
+        RAISE NOTICE 'Операция выполнена, rating_p установлен в 1, rating_p=%', 
             v_rating_p;
 
     END \$\$;
